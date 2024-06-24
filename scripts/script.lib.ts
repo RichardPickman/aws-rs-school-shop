c707495a590a9b86c7348718a4dc7ad52fb49716
RichardPickman <drezzerock@gmail.com>
2024-06-24 07:15:20 +0200

    update stack

--- scripts/script.lib.ts
+++ scripts/script.lib.ts
@@ -1,12 +1,29 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import path from 'path';
+import { PRODUCTS_TABLE_NAME, STOCK_TABLE_NAME } from '../constants';
 
 export class ProductsServiceStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const productsTable = new Table(this, PRODUCTS_TABLE_NAME, {
+            tableName: PRODUCTS_TABLE_NAME,
+            partitionKey: { name: 'id', type: AttributeType.STRING },
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+
+        const stocksTable = new Table(this, STOCK_TABLE_NAME, {
+            tableName: STOCK_TABLE_NAME,
+            partitionKey: {
+                name: 'product_id',
+                type: AttributeType.STRING,
+            },
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
 
         const getProductById = new Function(this, 'GetProductByIdHandler', {
             runtime: Runtime.NODEJS_20_X,
@@ -20,6 +37,12 @@ export class ProductsServiceStack extends Stack {
             handler: 'getProducts.handler',
         });
 
+        const createProduct = new Function(this, 'CreateProductHandler', {
+            runtime: Runtime.NODEJS_20_X,
+            code: Code.fromAsset(path.join(__dirname, '../lambda')),
+            handler: 'createProduct.handler',
+        });
+
         const api = new RestApi(this, 'ProductsService', {
             restApiName: 'ProductsService',
         });
@@ -27,7 +50,11 @@ export class ProductsServiceStack extends Stack {
         const productsEndpoit = api.root.addResource('products');
         const productByIdEndpoint = productsEndpoit.addResource('{id}');
 
+        productsEndpoit.addMethod('POST', new LambdaIntegration(createProduct));
         productsEndpoit.addMethod('GET', new LambdaIntegration(getProductsList));
         productByIdEndpoint.addMethod('GET', new LambdaIntegration(getProductById));
+
+        new CfnOutput(this, 'Products table', { value: productsTable.tableName });
+        new CfnOutput(this, 'Stock table', { value: stocksTable.tableName });
     }
 }
