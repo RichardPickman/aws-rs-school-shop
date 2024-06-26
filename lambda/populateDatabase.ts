fe5efe3d5acdb269f482a66921fd48a67ef0a6c1
Richard Pickman <drezzerock@gmail.com>
2024-06-26 23:23:36 +0200

    add transaction to populate

--- lambda/populateDatabase.ts
+++ lambda/populateDatabase.ts
@@ -1,5 +1,5 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, TransactWriteCommand, TransactWriteCommandInput } from '@aws-sdk/lib-dynamodb';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { randomUUID } from 'crypto';
 import { STOCK_TABLE_NAME } from '../constants';
@@ -11,60 +11,49 @@ export const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME || '';
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
 
-export const createStock = async (id: string) => {
-    const count = Math.floor(Math.random() * 100);
-
-    const stockCommand = new PutCommand({
-        TableName: STOCK_TABLE_NAME,
-        Item: {
-            product_id: id,
-            count,
-        },
-    });
-
-    try {
-        await docClient.send(stockCommand);
-
-        return { id, count };
-    } catch (err: unknown) {
-        const error = err as { message: string };
-
-        return { message: error.message };
-    }
-};
-
-export const createItem = async (item: Omit<Product, 'id'>) => {
+export const createProduct = async (item: Omit<Product, 'id'>) => {
     const { title, description, price } = item;
     const id = randomUUID();
+    const count = Math.floor(Math.random() * 100);
 
-    const productCommand = new PutCommand({
-        TableName: PRODUCTS_TABLE_NAME,
-        Item: {
-            id,
-            title,
-            description,
-            price,
-        },
-    });
+    const command: TransactWriteCommandInput = {
+        TransactItems: [
+            {
+                Put: {
+                    TableName: PRODUCTS_TABLE_NAME,
+                    Item: {
+                        id,
+                        title,
+                        description,
+                        price,
+                    },
+                },
+            },
+            {
+                Put: {
+                    TableName: STOCK_TABLE_NAME,
+                    Item: {
+                        product_id: id,
+                        count,
+                    },
+                },
+            },
+        ],
+    };
 
-    const product = await docClient.send(productCommand);
-    const stock = await createStock(id);
+    const transaction = new TransactWriteCommand(command);
+    const product = await docClient.send(transaction);
 
     return {
         ...item,
-        count: stock.count ?? 0,
+        count,
     };
 };
 
 export const handler = async (event: APIGatewayProxyEvent) => {
     console.log('Populating database');
 
-    const promisedItems = products.map(async (item) => {
-        const product = await createItem(item);
-
-        return product;
-    });
-
+    const promisedItems = products.map(createProduct);
     const result = await Promise.all(promisedItems);
 
     return {
