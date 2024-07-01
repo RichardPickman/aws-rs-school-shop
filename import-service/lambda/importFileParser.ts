80a272bd79361c946a88f4a528720d93b85257e2
Richard Pickman <drezzerock@gmail.com>
2024-07-01 17:51:00 +0200

    update code

--- import-service/lambda/importFileParser.ts
+++ import-service/lambda/importFileParser.ts
@@ -1,7 +1,8 @@
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { DeleteObjectCommand, GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { S3Event } from 'aws-lambda';
 import csvParser from 'csv-parser';
 import { Readable } from 'stream';
+import { ReadableStream } from 'stream/web';
 
 export const handler = async (event: S3Event) => {
     console.log('Logging the new csv intestines: ', event.Records);
@@ -20,14 +21,36 @@ export const handler = async (event: S3Event) => {
             return;
         }
 
-        console.log('Creating a readable stream');
-
-        const stream = response.Body.transformToWebStream() as import('stream/web').ReadableStream<any>;
+        const stream = response.Body.transformToWebStream() as ReadableStream<any>;
 
         Readable.fromWeb(stream)
             .pipe(csvParser())
-            .on('data', (data) => {
-                console.log('Data: ', data);
+            .on('data', (data) => console.log('Data: ', data))
+            .on('error', (err) => console.log('Error: ', err))
+            .on('end', async () => {
+                try {
+                    console.log('End of stream');
+
+                    const parsedKey = `parsed/${key.split('/').pop()}`;
+
+                    const copyParams = {
+                        Bucket: bucket,
+                        Key: parsedKey,
+                    };
+
+                    await client
+                        .send(new PutObjectCommand(copyParams))
+                        .then((data) => console.log('Successfully copied object'))
+                        .catch((err) => console.log('Error occured while copying object', err));
+                    await client
+                        .send(new DeleteObjectCommand({ Bucket: bucket, Key: key }))
+                        .then((data) => console.log('Successfully deleted object'))
+                        .catch((err) => console.log('Error occured while deleting object', err));
+
+                    console.log(`File moved to ${parsedKey}`);
+                } catch (err) {
+                    console.log('Error occured while processing file', err);
+                }
             });
     }
 };
--- import-service/test/importProductsFile.test.ts
+++ import-service/test/importProductsFile.test.ts
@@ -1,21 +1,30 @@
-import * as s3RequestPresigner from '@aws-sdk/s3-request-presigner';
+jest.mock('@aws-sdk/s3-request-presigner');
+import { GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+jest.mock('@aws-sdk/client-s3');
+
+import { APIGatewayProxyEvent } from 'aws-lambda';
 import { handler } from '../lambda/importProductsFile';
 
 describe('getProductById', () => {
     it('Should pass all included tests with valid data', async () => {
-        const event = {
-            queryParams: {
+        const event: Partial<APIGatewayProxyEvent> = {
+            queryStringParameters: {
                 name: 'proxy-image.jpg',
             },
             httpMethod: 'GET',
         };
 
+        const getSignedUrlMock: jest.Mock = getSignedUrl as any;
+
         const image = 'https://test.com/proxy-image.jpg';
 
-        jest.spyOn(s3RequestPresigner, 'getSignedUrl').mockResolvedValue(image);
+        getSignedUrlMock.mockResolvedValue(image);
 
         const response = await handler(event);
 
+        expect(GetObjectCommand).toHaveBeenCalledTimes(1);
         expect(response.statusCode).toBe(201);
         expect(response.body).toBe(image);
     });
