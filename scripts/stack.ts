4438ea3b8d0766cc3f0ad071a79eebecbe71c091
Richard Pickman <drezzerock@gmail.com>
2024-07-04 19:13:37 +0200

    uncomment ProductServiceStack from stack creator

--- scripts/stack.ts
+++ scripts/stack.ts
@@ -3,7 +3,8 @@
 import * as cdk from 'aws-cdk-lib';
 import 'source-map-support/register';
 import { ImportServiceStack } from '../import-service/stack/import-service.lib';
+import { ProductsServiceStack } from '../product-service/stack/product-service.lib';
 
 const app = new cdk.App();
-// new ProductsServiceStack(app, 'ProductsServiceStack');
+new ProductsServiceStack(app, 'ProductsServiceStack');
 new ImportServiceStack(app, 'ImportServiceStack');
