b5dcfb3878dabd2435cf15ad06c214c7dd0bc2cc
Richard Pickman <drezzerock@gmail.com>
2024-07-04 19:13:08 +0200

    add product parser from csv to dynamoDB item

---
+++ product-service/lambda/catalogBatchProcess.ts
@@ -0,0 +1,70 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { SNSClient, SubscribeCommand } from '@aws-sdk/client-sns';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { SQSEvent } from 'aws-lambda';
+import { randomUUID } from 'crypto';
+import { Product } from '../../types';
+
+type RawProduct = Omit<Product, 'id'>;
+
+const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME || '';
+const TOPIC_ARN = process.env.TOPIC_ARN || '';
+const SUBSCRIPTION_EMAIL = process.env.SUBSCRIPTION_EMAIL || '';
+
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
+
+const snsClient = new SNSClient();
+
+export const handler = async (event: SQSEvent) => {
+    console.log('Processing catalog items...');
+
+    const products: RawProduct[] = event.Records.map((record) => JSON.parse(record.body));
+
+    for (const product of products) {
+        const { title, price, description, stock } = product;
+
+        console.log(`Processing ${title}...`);
+
+        const productCommand = new PutCommand({
+            TableName: PRODUCTS_TABLE_NAME,
+            Item: {
+                id: randomUUID(),
+                title,
+                description,
+                price,
+                stock,
+            },
+        });
+
+        console.log('Sending product command with title: ', title);
+
+        await docClient.send(productCommand);
+
+        console.log('Product ' + title + ' created');
+    }
+
+    console.log('Products created');
+
+    try {
+        const catalogSubscription = new SubscribeCommand({
+            TopicArn: TOPIC_ARN,
+            Protocol: 'email',
+            Endpoint: SUBSCRIPTION_EMAIL,
+        });
+
+        console.log('Sending catalog subscription...');
+
+        await snsClient.send(catalogSubscription);
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify({ message: `Products created and subscription created` }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: `Error processing event: ${error}` }),
+        };
+    }
+};
--- product-service/stack/product-service.lib.ts
+++ product-service/stack/product-service.lib.ts
@@ -1,17 +1,38 @@
 import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { AccountPrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 import path from 'path';
-import { PRODUCTS_TABLE_NAME, STOCK_TABLE_NAME } from '../../constants';
+import { PRINCIPAL_ID, PRODUCTS_TABLE_NAME, STOCK_TABLE_NAME, SUBSCRIPTION_EMAIL } from '../../constants';
+
+const rootDir = path.join(__dirname, '../../');
+
+const commonLambdaProps = {
+    runtime: Runtime.NODEJS_20_X,
+    projectRoot: rootDir,
+    depsLockFilePath: path.join(rootDir, 'pnpm-lock.yaml'),
+    environment: {
+        PRODUCTS_TABLE_NAME: PRODUCTS_TABLE_NAME,
+        STOCK_TABLE_NAME: STOCK_TABLE_NAME,
+    },
+    bundling: {
+        externalModules: ['aws-sdk'],
+        minify: false,
+    },
+};
 
 export class ProductsServiceStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
+        // Product service tables.
         const productsTable = new Table(this, PRODUCTS_TABLE_NAME, {
             tableName: PRODUCTS_TABLE_NAME,
             partitionKey: { name: 'id', type: AttributeType.STRING },
@@ -27,92 +48,97 @@ export class ProductsServiceStack extends Stack {
             removalPolicy: RemovalPolicy.DESTROY,
         });
 
+        // Topic for creating products.
+        const createProductTopic = new Topic(this, 'CreateProductTopic', {
+            topicName: 'CreateProductTopic',
+            displayName: 'CreateProductTopic',
+        });
+
+        // Lambda functions.
+        const getProductById = new NodejsFunction(this, 'GetProductByIdHandler', {
+            ...commonLambdaProps,
+            entry: path.join(rootDir, '/product-service/lambda/getProductById.ts'),
+        });
+
+        const getProductsList = new NodejsFunction(this, 'GetProductsHandler', {
+            ...commonLambdaProps,
+            entry: path.join(rootDir, '/product-service/lambda/getProducts.ts'),
+        });
+
+        const createProduct = new NodejsFunction(this, 'CreateProductHandler', {
+            ...commonLambdaProps,
+            entry: path.join(rootDir, '/product-service/lambda/createProduct.ts'),
+        });
+
+        const populateProduct = new NodejsFunction(this, 'PopulateProductHandler', {
+            ...commonLambdaProps,
+            entry: path.join(rootDir, '/product-service/lambda/populateDatabase.ts'),
+        });
+
+        const catalogItems = new NodejsFunction(this, 'CatalogItemsHandler', {
+            ...commonLambdaProps,
+            environment: {
+                ...commonLambdaProps.environment,
+                TOPIC_ARN: createProductTopic.topicArn,
+                SUBSCRIPTION_EMAIL: SUBSCRIPTION_EMAIL,
+            },
+            entry: path.join(rootDir, '/product-service/lambda/catalogBatchProcess.ts'),
+        });
+
+        // Queue for catalog items.
+        const catalogItemsQueue = new Queue(this, 'CatalogItemsQueue', {
+            queueName: 'CatalogItemsQueue',
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+
+        // Principal for the topic policy.
+        const accessPrincipal = new AccountPrincipal(PRINCIPAL_ID);
+
+        // Policy for the lambda functions.
+        const productTopicPolicy = new PolicyStatement({
+            actions: ['sns:Subscribe'],
+            resources: [createProductTopic.topicArn],
+            principals: [accessPrincipal],
+        });
+
         const dbPolicy = new PolicyStatement({
             actions: ['dynamodb:Scan', 'dynamodb:GetItem', 'dynamodb:PutItem'],
             resources: [productsTable.tableArn, stocksTable.tableArn],
         });
 
-        const rootDir = path.join(__dirname, '../../');
+        // Event source for the catalog items.
+        catalogItems.addEventSource(new SqsEventSource(catalogItemsQueue, { batchSize: 5 }));
 
-        const getProductById = new NodejsFunction(this, 'GetProductByIdHandler', {
-            runtime: Runtime.NODEJS_20_X,
-            projectRoot: rootDir,
-            depsLockFilePath: path.join(rootDir, 'pnpm-lock.yaml'),
-            environment: {
-                PRODUCTS_TABLE_NAME: PRODUCTS_TABLE_NAME,
-                STOCK_TABLE_NAME: STOCK_TABLE_NAME,
-            },
-            bundling: {
-                externalModules: ['aws-sdk'],
-                minify: false,
-            },
-            entry: path.join(rootDir, '/product-service/lambda/getProductById.ts'),
-        });
-
-        const getProductsList = new NodejsFunction(this, 'GetProductsHandler', {
-            runtime: Runtime.NODEJS_20_X,
-            projectRoot: rootDir,
-            depsLockFilePath: path.join(rootDir, 'pnpm-lock.yaml'),
-            environment: {
-                PRODUCTS_TABLE_NAME: PRODUCTS_TABLE_NAME,
-                STOCK_TABLE_NAME: STOCK_TABLE_NAME,
-            },
-            bundling: {
-                externalModules: ['aws-sdk'],
-                minify: false,
-            },
-            entry: path.join(rootDir, '/product-service/lambda/getProducts.ts'),
-        });
-
-        const createProduct = new NodejsFunction(this, 'CreateProductHandler', {
-            runtime: Runtime.NODEJS_20_X,
-            projectRoot: rootDir,
-            depsLockFilePath: path.join(rootDir, 'pnpm-lock.yaml'),
-            environment: {
-                PRODUCTS_TABLE_NAME: PRODUCTS_TABLE_NAME,
-                STOCK_TABLE_NAME: STOCK_TABLE_NAME,
-            },
-            bundling: {
-                externalModules: ['aws-sdk'],
-                minify: false,
-            },
-            entry: path.join(rootDir, '/product-service/lambda/createProduct.ts'),
-        });
-
-        const populateProduct = new NodejsFunction(this, 'PopulateProductHandler', {
-            runtime: Runtime.NODEJS_20_X,
-            projectRoot: rootDir,
-            depsLockFilePath: path.join(rootDir, 'pnpm-lock.yaml'),
-            environment: {
-                PRODUCTS_TABLE_NAME: PRODUCTS_TABLE_NAME,
-                STOCK_TABLE_NAME: STOCK_TABLE_NAME,
-            },
-            bundling: {
-                externalModules: ['aws-sdk'],
-                minify: false,
-            },
-            entry: path.join(rootDir, '/product-service/lambda/populateDatabase.ts'),
-        });
-
+        // Add permissions to the lambda functions.
+        createProductTopic.addToResourcePolicy(productTopicPolicy);
         getProductById.addToRolePolicy(dbPolicy);
         getProductsList.addToRolePolicy(dbPolicy);
         createProduct.addToRolePolicy(dbPolicy);
         populateProduct.addToRolePolicy(dbPolicy);
+        catalogItems.addToRolePolicy(dbPolicy);
 
+        // Add subscription to the topic.
+        createProductTopic.addSubscription(new EmailSubscription(SUBSCRIPTION_EMAIL));
+
+        // API Gateway.
         const api = new RestApi(this, 'ProductsService', {
             restApiName: 'ProductsService',
         });
 
+        // API Gateway resources.
         const productsEndpoit = api.root.addResource('products');
         const populateEndpoit = api.root.addResource('populate');
         const productByIdEndpoint = productsEndpoit.addResource('{productId}');
 
+        // API Gateway methods.
         productsEndpoit.addMethod('POST', new LambdaIntegration(createProduct));
         productsEndpoit.addMethod('GET', new LambdaIntegration(getProductsList));
         productByIdEndpoint.addMethod('GET', new LambdaIntegration(getProductById));
         populateEndpoit.addMethod('POST', new LambdaIntegration(populateProduct));
 
+        // Outputs.
         new CfnOutput(this, 'Products table', { value: productsTable.tableName });
+        new CfnOutput(this, 'Queue Url', { value: catalogItemsQueue.queueUrl });
         new CfnOutput(this, 'Stock table', { value: stocksTable.tableName });
     }
 }
