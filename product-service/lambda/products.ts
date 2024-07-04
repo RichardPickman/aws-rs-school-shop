e3ac1ae5eb3c60ea3f9fcaab6987d0c678b78444
Richard Pickman <drezzerock@gmail.com>
2024-07-04 21:53:45 +0200

    add stock value to products list

--- product-service/lambda/products.ts
+++ product-service/lambda/products.ts
@@ -3,20 +3,24 @@ export const products = [
         title: 'Product 1',
         price: 100,
         description: 'This is a product',
+        stock: 10,
     },
     {
         title: 'Product 2',
         price: 200,
         description: 'This is another product',
+        stock: 20,
     },
     {
         title: 'Product 3',
         price: 300,
         description: 'This is the third product',
+        stock: 30,
     },
     {
         title: 'Product 4',
         price: 400,
         description: 'This is the fourth product',
+        stock: 40,
     },
 ];
