15007fc68251f5b93ac37c7c6fc2903cb2674307
Richard Pickman <drezzerock@gmail.com>
2024-08-09 13:35:41 +0200

    add types for helpers

--- scripts/stacks/helpers.ts
+++ scripts/stacks/helpers.ts
@@ -1,9 +1,10 @@
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import path from 'path';
 import { PRODUCTS_TABLE_NAME, STOCK_TABLE_NAME } from '../../constants';
 
 export const rootDir = path.join(__dirname, '../../');
-export const commonLambdaProps = {
+export const commonLambdaProps: NodejsFunctionProps = {
     runtime: Runtime.NODEJS_20_X,
     projectRoot: rootDir,
     depsLockFilePath: path.join(rootDir, 'pnpm-lock.yaml'),
