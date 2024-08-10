13c15e3a02ddcc3a317ee5ded0abde890d119c07
Richard Pickman <drezzerock@gmail.com>
2024-08-10 16:21:59 +0200

    reduce code amount

--- services/bff/src/app.controller.ts
+++ services/bff/src/app.controller.ts
@@ -8,56 +8,56 @@ import {
 import { Request } from 'express';
 import { getConfig, recipients } from './mappers';
 
+const handleFetch = async (url: string, config: RequestInit) => {
+  try {
+    const request = await fetch(url, config);
+
+    if (request.status >= 400) {
+      console.log('Backend responded with error', request.status);
+
+      throw new HttpException(
+        'Endpoint returned error',
+        HttpStatus.BAD_GATEWAY,
+      );
+    }
+
+    return request.json();
+  } catch (error) {
+    console.log('Backend responded with error', error);
+
+    throw new HttpException('Endpoint returned error', HttpStatus.BAD_GATEWAY);
+  }
+};
+
 @Controller('*')
 export class AppController {
   constructor() {}
-
-  private async handleCart(@Req() req: Request) {
-    const config = getConfig(req.method, req.body, req.headers);
-
-    const request = await fetch(recipients.cart, config);
-
-    if (request.status > 299) {
-      console.log('Backend responded with error', request.status);
-
-      throw new HttpException(
-        'Endpoint returned error',
-        HttpStatus.BAD_GATEWAY,
-      );
-    }
-
-    return request.json();
-  }
-
-  private async handleProduct(@Req() req: Request) {
-    const config = getConfig(req.method, req.body, req.headers);
-
-    const request = await fetch(recipients.product, config);
-
-    if (request.status > 299) {
-      console.log('Backend responded with error', request.status);
-
-      throw new HttpException(
-        'Endpoint returned error',
-        HttpStatus.BAD_GATEWAY,
-      );
-    }
-
-    return request.json();
-  }
 
   @All()
   handleEndpoint(@Req() req: Request) {
     const [path] = req.url.split('?');
     const paths = path.split('/').filter(Boolean);
-    const [cart, product] = Object.keys(recipients);
+    const [cart, cart_checkout, product] = Object.keys(recipients);
 
     if (paths[0] === cart) {
-      return this.handleCart(req);
+      return handleFetch(
+        recipients.cart,
+        getConfig(req.method, req.body, req.headers),
+      );
+    }
+
+    if (paths[0] === cart_checkout) {
+      return handleFetch(
+        recipients.cart_checkout,
+        getConfig(req.method, req.body, req.headers),
+      );
     }
 
     if (paths[0] === product) {
-      return this.handleProduct(req);
+      return handleFetch(
+        recipients.product,
+        getConfig(req.method, req.body, req.headers),
+      );
     }
 
     throw new HttpException('Cannot process request', HttpStatus.BAD_GATEWAY);
